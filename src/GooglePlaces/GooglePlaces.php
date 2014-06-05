d1b6ac8186783d857fe766d28dc5228bb9884d7a
76200 <bartlomiej.marek.czyz@gmail.com>
2014-06-05 15:58:09 +0200

    API key passed through constructor

--- src/GooglePlaces/GooglePlaces.php
+++ src/GooglePlaces/GooglePlaces.php
@@ -2,7 +2,34 @@
 
 namespace bart\GooglePlaces;
 
+use bart\GooglePlaces\Exception\APIKeyException;
+
+/**
+ * Class GooglePlaces
+ */
 class GooglePlaces
 {
+    /**
+     * @var string
+     */
+    private $apiKey;
+
+    /**
+     * Constructor.
+     *
+     * @param string $apiKey
+     * @throws \InvalidArgumentException | APIKeyException
+     */
+    function __construct($apiKey)
+    {
+        if(!is_string($apiKey)) {
+            throw new \InvalidArgumentException('Wrong API key format');
+        }
+        if (!trim($apiKey) || !is_string($apiKey)) {
+            throw new APIKeyException('Empty API key');
+        }
+
+        $this->apiKey = $apiKey;
+    }
 
 }
