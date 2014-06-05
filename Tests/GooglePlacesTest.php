e9ab8a8642e600fea75928bc15d22f01d81144c0
76200 <bartlomiej.marek.czyz@gmail.com>
2014-06-05 15:59:17 +0200

    Added tests

---
+++ Tests/GooglePlacesTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use bart\GooglePlaces\GooglePlaces;
+
+/**
+ * Class GooglePlacesTest
+ */
+class GooglePlacesTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @expectedException bart\GooglePlaces\Exception\APIKeyException
+     */
+    public function testEmptyConstructor()
+    {
+        new GooglePlaces('');
+    }
+
+    /**
+     * @expectedException bart\GooglePlaces\Exception\APIKeyException
+     */
+    public function testEmptyConstructorWhitespaces()
+    {
+        new GooglePlaces(' ');
+    }
+
+    /**
+     * @expectedException \InvalidArgumentException
+     */
+    public function testConstructorInvalidArgument()
+    {
+        new GooglePlaces([]);
+    }
+
+    public function testConstructor()
+    {
+
+    }
+}
